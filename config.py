8715d01df692c340668c7b17a9cecbee2b8609ef
udaymitra <udaymitra4u@gmail.com>
2017-02-05 12:12:20 -0800

    equal distribution

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 CONFIG = {
     'camera_correction': 0.2,
     'steering_augmentation_sigma': 0.1,
-    'num_training_entries_per_image': 10,
+    'num_training_entries_per_image': 5,
     'training_data_split': 0.75,
     'steering_bin_size': 0.05,
     'augmentation_value_min': 0.2,
